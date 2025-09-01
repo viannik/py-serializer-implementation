acbffa4fde0f1a4be01bf8ca7364877f8421f6d2
viannik <ivan.nikitski@gmail.com>
2025-09-02 02:11:00 +0300

    Solution

---
+++ car/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = "car.apps.SerializerAppConfig"
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from car.models import Car
+from car.serializers import CarSerializer
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+import io
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(instance=car)
+    return JSONRenderer().render(serializer.data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
